3766f70a13dd71c229353c00d0300a7fc6270ffb
Dmitriy Panivnyk <d.panivnyk@ukr.net>
2023-01-02 12:25:26 +0200

    add some style's

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
+import React, { useState } from "react";
+import { AppLoading } from "expo";
 
 import RegistrationScreen from "./Screens/RegistrationScreen/RegistrationScreen";
 import LoginScreen from "./Screens/LoginScreen/LoginScreen";
 
+// const loadFonts = async () => {
+//   await Font.loadAsync({
+//     "Roboto-Regular": require("./assets/fonts/Roboto/Roboto-Regular.ttf"),
+//     "Roboto-Bold": require("./assets/fonts/Roboto/Roboto-Bold.ttf"),
+//     "Roboto-BoldItalic": require("./assets/fonts/Roboto/Roboto-BoldItalic.ttf"),
+//   });
+// };
+
 export default function App() {
+  // const [isReady, setIsReady] = useState(false);
+  // if (!isReady) {
+  //   return (
+  //     <AppLoading startAsync={loadFonts} onFinish={() => setIsReady(true)} />
+  //   );
+  // }
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
@@ -16,8 +33,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    //   backgroundColor: "#fff",
+    //   alignItems: "center",
+    //   justifyContent: "center",
   },
 });
--- Screens/RegistrationScreen/RegistrationScreen.js
+++ Screens/RegistrationScreen/RegistrationScreen.js
@@ -16,7 +16,7 @@ import {
   KeyboardAvoidingView,
   Platform,
   Alert,
-  Button,
+  TouchableOpacity,
   Text,
 } from "react-native";
 
@@ -38,38 +38,44 @@ export default function App() {
       <View style={styles.container}>
         <ImageBackground
           style={styles.image}
-          source={require("../../img/photoBG.jpg")}
+          resizeMode="cover"
+          source={require("../../assets/img/photoBG.jpg")}
         >
           <KeyboardAvoidingView
             behavior={Platform.OS == "ios" ? "padding" : "height"}
           >
-            <Text style={styles.text}>Registration</Text>
-            <TextInput
-              value={login}
-              onChangeText={loginHandler}
-              placeholder="Login"
-              style={styles.input}
-            />
-            <TextInput
-              value={email}
-              onChangeText={emailHandler}
-              placeholder="E-mail"
-              secureTextEntry={true}
-              style={styles.input}
-            />
-            <TextInput
-              value={password}
-              onChangeText={passwordHandler}
-              placeholder="Password"
-              secureTextEntry={true}
-              style={styles.input}
-            />
-            <Button
-              title={"Registration"}
-              style={styles.input}
-              onPress={onRegistration}
-            />
-            <Text style={styles.text}>Already have an account? LogIn</Text>
+            <View style={styles.form}>
+              <Text style={styles.textTitle}>Registration</Text>
+              <TextInput
+                value={login}
+                onChangeText={loginHandler}
+                placeholder="Login"
+                style={styles.input}
+              />
+              <TextInput
+                value={email}
+                onChangeText={emailHandler}
+                placeholder="E-mail"
+                secureTextEntry={true}
+                style={styles.input}
+              />
+              <TextInput
+                value={password}
+                onChangeText={passwordHandler}
+                placeholder="Password"
+                secureTextEntry={true}
+                style={styles.input}
+              />
+
+              <TouchableOpacity
+                activeOpacity={0.7}
+                style={styles.button}
+                // onPress={handleSubmit}
+              >
+                <Text style={styles.btnTitle}>Sign up</Text>
+              </TouchableOpacity>
+              <Text style={styles.text}>Already have an account? LogIn</Text>
+            </View>
           </KeyboardAvoidingView>
         </ImageBackground>
       </View>
@@ -78,31 +84,77 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  image: {
-    flex: 1,
-    justifyContent: "flex-end",
-  },
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundImage: "../../img/photoBG.jpg",
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "flex-end",
+  },
+  textTitle: {
+    fontFamily: "Roboto-Bold",
+    fontWeight: 500,
+    fontSize: 30,
+    lineHeight: 35,
+    letterSpacing: 0.01,
+    color: "#212121",
+    textAlign: "center",
+    marginBottom: 33,
+    // justifyContent: "center",
+    // alignItems: "center",
+  },
+  form: {
+    // position: "relative",
+    width: "100%",
+    paddingHorizontal: 16,
+    paddingTop: 92,
+    backgroundColor: "#fff",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    // marginHorizontal: 40,
   },
   input: {
-    width: 250,
-    marginLeft: 16,
-    marginRight: 16,
+    fontFamily: "Roboto-Regular",
+    // width: "100%",
+    // marginLeft: 0,
+    // marginRight: 0,
+    fontSize: 16,
+    lineHeight: 19,
+    height: 50,
+    marginBottom: 16,
+    borderWidth: 1,
     height: 50,
     padding: 10,
     borderWidth: 1,
     borderColor: "#E8E8E8",
+    backgroundColor: "#F6F6F6",
     borderRadius: 8,
     marginBottom: 10,
+    color: "#212121",
   },
+  button: {
+    height: 51,
+    backgroundColor: "#FF6C00",
+    borderRadius: 100,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  btnTitle: {
+    fontFamily: "Roboto-Regular",
+    fontSize: 16,
+    lineHeight: 19,
+    color: "#fff",
+  },
+
   text: {
     fontSize: 16,
     lineHeight: 19,
     fontFamily: "Roboto-Regular",
     textAlign: "center",
+    marginTop: 16,
+    marginBottom: 66,
   },
 });
